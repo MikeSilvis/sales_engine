a8e9b2e9e3dd851159477e65ff9d94abae2ac9bf
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-03-20 15:11:32 -0400

    initial scaffolding of classes

---
+++ lib/sales_engine/Item.rb
@@ -0,0 +1,18 @@
+module SalesEngine
+  class Item
+    attr_accessor :id, :name, :description, :unit_price, :merchant_id, 
+                  :created_at, :updated_at
+    def self.most_revenue(limit)
+      
+    end
+
+    def self.most_items(limit)
+      
+    end
+    def best_day
+      
+    end
+
+  end
+
+end
---
+++ lib/sales_engine/Merchant.rb
@@ -0,0 +1,44 @@
+module SalesEngine
+
+  class Merchant
+    ## Has_many :invoices, :items
+    attr_accessor :id, :name, :created_at, :updated_at
+    def self.most_revenue
+
+    end
+
+    def selfmost_items
+
+    end
+
+    def self.revenue(date)
+
+    end
+
+    def revenue
+
+    end
+
+    def favorite_customer
+
+    end
+
+    def customers_with_pending_invoices
+
+    end
+
+    def random
+      
+    end
+
+    def find_by_x(match)
+      
+    end
+
+    def find_all_by_x(match)
+      
+    end
+
+  end
+
+end
