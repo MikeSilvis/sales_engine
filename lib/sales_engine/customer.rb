9f5a5e41a96a797b9d61bfd0693040e7267f3d0a
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-03-21 07:47:40 -0400

    adding files

---
+++ lib/sales_engine/customer.rb
@@ -0,0 +1,25 @@
+module SalesEngine
+  class Customer
+    attr_accessor :id, :first_name, :last_name, :created_at, :updated_at
+    #has_many :invoices
+
+    def days_since_activity
+
+    end
+
+    def pending_invoices
+
+    end
+
+    def selfmost_items
+
+    end
+
+    def self.most_revenue
+
+    end
+
+  end
+
+
+end
